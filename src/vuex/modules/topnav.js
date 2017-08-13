a1853c962450dc73455e3c0f158c21a093ce82ae
You <you85630@foxmail.com>
2017-08-13 15:31:39 +0800

    修改

--- src/vuex/modules/topnav.js
+++ src/vuex/modules/topnav.js
@@ -1,15 +1,13 @@
-import local from 'assets/js/local'
 export default {
   state: {
     nowIndex: 0
   },
   getters: {
-    nowIndex: state => state.nowIndex ? state.nowIndex : local.getItem('leftNav')
+    nowIndex: state => state.nowIndex
   },
   mutations: {
     reset (state, index) {
       state.nowIndex = index
-      local.setItem('leftNav', index)
     }
   },
   actions: {
