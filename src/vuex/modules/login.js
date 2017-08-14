3b6a10922fbfe2c0508f6727305a1bce3878ad69
You <you85630@foxmail.com>
2017-08-14 16:50:47 +0800

    修改

--- src/vuex/modules/login.js
+++ src/vuex/modules/login.js
@@ -1,5 +1,7 @@
 import * as types from './../mutations-types'
 import router from 'router'
+import api from 'assets/js/api'
+import qs from 'qs'
 
 export default {
   state: {
@@ -14,7 +16,8 @@ export default {
     msg: state => state.msg
   },
   mutations: {
-    [types.LOGIN] (state) {
+    [types.LOGIN] (state, res) {
+      localStorage.setItem('token', res.info.token)
       router.push('/index')
       state.form.username = ''
       state.form.password = ''
@@ -22,7 +25,17 @@ export default {
   },
   actions: {
     logIn: ({
-      commit
-    }, key) => commit('LOGIN', key)
+      commit, state
+    }, key) => {
+      api.post('https://www.easy-mock.com/mock/5962eeb29adc231f357c6bd5/sys/login', qs.stringify(key))
+      .then(function (res) {
+        if (res.data.code === 200) {
+          commit('LOGIN', res.data)
+          state.msg = ''
+        } else {
+          state.msg = res.data.message
+        }
+      })
+    }
   }
 }
